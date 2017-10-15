57016a2967709be8cf7d841f04215059d49b2e9c
Роман Зотов <gatt@inbox.ru>
2017-10-15 03:22:27 +0300

    Совместимость quicklz с gp

--- src/backend/catalog/quicklz.h
+++ src/backend/catalog/quicklz.h
@@ -22,9 +22,9 @@
 
 	// 1 gives fastest compression speed. 3 gives fastest decompression speed and best
 	// compression ratio. 
-	//#define QLZ_COMPRESSION_LEVEL 1
+        #define QLZ_COMPRESSION_LEVEL 1
 	//#define QLZ_COMPRESSION_LEVEL 2
-	#define QLZ_COMPRESSION_LEVEL 3
+        //#define QLZ_COMPRESSION_LEVEL 3
 
 	// If > 0, zero out both states prior to first call to qlz_compress() or qlz_decompress() 
 	// and decompress packets in the same order as they were compressed
@@ -35,7 +35,7 @@
 
 	// Guarantees that decompression of corrupted data cannot crash. Decreases decompression
 	// speed 10-20%. Compression speed not affected.
-	#define QLZ_MEMORY_SAFE
+	//#define QLZ_MEMORY_SAFE
 #endif
 
 #define QLZ_VERSION_MAJOR 1
