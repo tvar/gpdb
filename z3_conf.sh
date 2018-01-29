3be250553f1b77f44ec006be9b61678d859bc4a0
gatt <gatt@inbox.ru>
2018-01-29 11:53:22 +0100

    zstd

--- z3_conf.sh
+++ z3_conf.sh
@@ -1,5 +1,6 @@
 LD_LIBRARY_PATH=$(pwd)/depends/build/lib ./configure \
     --enable-debug \
+    --with-zstd \
     --with-libraries=$(pwd)/depends/build/lib \
     --with-includes=$(pwd)/depends/build/include \
     --with-libxml --with-gssapi --prefix=/usr/local/gpdb/gpdb-$(date '+%Y-%m-%d')
